6e4405b37e1387b2909de4418e712dd00e2612d2
clmntd <62601115+clmntd@users.noreply.github.com>
2022-10-26 01:24:10 +1100

    readability

--- src/components/Desktop1.js
+++ src/components/Desktop1.js
@@ -3,43 +3,43 @@ import './Desktop1.css'
 export default function Desktop1 () {
 	return (
 		<div className='Desktop1_Desktop1'>
-			<div className='Group4'>
+			<div className='Group1'>
 				<div className='Rectangle1'/>
-				<span className='Subject'>Subject</span>
+				<span className='Subject_0'>Subject</span>
 			</div>
-			<div className='Group6'>
-				<div className='Rectangle15'/>
+			<div className='Group2'>
+				<div className='Rectangle2'/>
 				<span className='Subject_1'>Subject</span>
 			</div>
-			<div className='Group5'>
-				<div className='Rectangle13'/>
+			<div className='Group3'>
+				<div className='Rectangle3'/>
 				<span className='Subject_2'>Subject</span>
 			</div>
-			<div className='Group7'>
-				<div className='Rectangle14'/>
+			<div className='Group4'>
+				<div className='Rectangle4'/>
 				<span className='Subject_3'>Subject</span>
 			</div>
-			<div className='Rectangle7'/>
+			<div className='Rectangle5'/>
 			<span className='Course'>Course</span>
-			<div className='Rectangle10'/>
+			<div className='Rectangle6'/>
 			<span className='Back'>Back</span>
-			<div className='Rectangle11'/>
+			<div className='Rectangle7'/>
 			<span className='LogOut'>Log Out</span>
-			<div className='Rectangle9'/>
-			<span className='Specialization'>Specialization</span>
 			<div className='Rectangle8'/>
+			<span className='Specialization'>Specialization</span>
+			<div className='Rectangle9'/>
 			<span className='YearofStudy'>Year of Study</span>
-			<div className='Group2'>
-				<div className='Rectangle10_1'/>
+			<div className='Group5'>
+				<div className='Rectangle10'/>
 				<span className='YearofStudy_1'>Year “” of Study</span>
 			</div>
-			<div className='Group1'>
-				<div className='Rectangle11_1'/>
+			<div className='Group6'>
+				<div className='Rectangle11'/>
 				<span className='Year1ofStudy'>Year “-1” of Study</span>
 			</div>
-			<div className='Group3'>
+			<div className='Group7'>
 				<div className='Rectangle12'/>
-				<span className='Year1ofStudy_1'>Year “+1” of Study</span>
+				<span className='Year1ofStudy'>Year “+1” of Study</span>
 			</div>
 		</div>
 	)
