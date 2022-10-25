f50f124cf3923bf36b1f6061abea5d35b05c2d57
clmntd <62601115+clmntd@users.noreply.github.com>
2022-10-26 01:10:09 +1100

    course rec page

---
+++ src/components/Desktop1.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import './Desktop1.css'
+export default function Desktop1 () {
+	return (
+		<div className='Desktop1_Desktop1'>
+			<div className='Group4'>
+				<div className='Rectangle1'/>
+				<span className='Subject'>Subject</span>
+			</div>
+			<div className='Group6'>
+				<div className='Rectangle15'/>
+				<span className='Subject_1'>Subject</span>
+			</div>
+			<div className='Group5'>
+				<div className='Rectangle13'/>
+				<span className='Subject_2'>Subject</span>
+			</div>
+			<div className='Group7'>
+				<div className='Rectangle14'/>
+				<span className='Subject_3'>Subject</span>
+			</div>
+			<div className='Rectangle7'/>
+			<span className='Course'>Course</span>
+			<div className='Rectangle10'/>
+			<span className='Back'>Back</span>
+			<div className='Rectangle11'/>
+			<span className='LogOut'>Log Out</span>
+			<div className='Rectangle9'/>
+			<span className='Specialization'>Specialization</span>
+			<div className='Rectangle8'/>
+			<span className='YearofStudy'>Year of Study</span>
+			<div className='Group2'>
+				<div className='Rectangle10_1'/>
+				<span className='YearofStudy_1'>Year “” of Study</span>
+			</div>
+			<div className='Group1'>
+				<div className='Rectangle11_1'/>
+				<span className='Year1ofStudy'>Year “-1” of Study</span>
+			</div>
+			<div className='Group3'>
+				<div className='Rectangle12'/>
+				<span className='Year1ofStudy_1'>Year “+1” of Study</span>
+			</div>
+		</div>
+	)
+}
